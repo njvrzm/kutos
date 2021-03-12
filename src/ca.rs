d57b2e06046b610ac11294ea2271719046cbf611
Nathan Verzemnieks <njvrzm@gmail.com>
2021-03-11 22:18:50 -0800

    A long-overdue checkpoint

--- src/ca.rs
+++ src/ca.rs
@@ -3,63 +3,95 @@ use rand::distributions::Standard;
 pub struct World {
     width: usize,
     height: usize,
-    size: usize,
     cells: Vec<bool>,
+    delta: Vec<i8>,
     count: Vec<i8>,
-    heat: Vec<i8>,
+    last_change: Vec<i32>,
     neighborhood: Vec<(i32, i32)>,
+    generation: i32,
 }
+
+pub enum State {
+    Unknown,
+    Interesting,
+    Dead,
+    Exploding,
+}
+
 impl World {
     pub fn new(width: usize, height: usize, neighborhood: Vec<(i32, i32)>) -> Self {
-        Self {
+        Self{
             width,
             height,
-            size: width * height,
             cells: vec![false; width * height],
+            delta: vec![0; width * height],
             count: vec![0; width * height],
-            heat: vec![0; width * height],
+            last_change: vec![0; width * height],
             neighborhood,
+            generation: 0,
         }
+    }
+    pub fn reset(&mut self) {
+        *self = Self::new(self.width, self.height, self.neighborhood.clone());
     }
     pub fn randomize(&mut self, fill: f32) {
         let mut rng = StdRng::seed_from_u64(32);
-        self.cells = vec![false; self.width * self.height];
-        for index in 0..self.size {
-            self.cells[index] = fill > rng.sample(Standard);
-
-        }
-        self.reset_count();
-    }
-    fn reset_count(&mut self) {
-        self.count = vec![0; self.width * self.height];
+        self.reset();
         for x in 0..self.width {
             for y in 0..self.height {
-                self.update_count(x, y, false, self.cells[x + y * self.width])
+                if fill > rng.sample(Standard) {
+                    self.set_cell_state(x, y, true)
+                }
             }
         }
     }
-    fn update_count(&mut self, x: usize, y:usize, old_state: bool, new_state: bool) {
-        if old_state != new_state {
-            for (nx, ny) in self.neighbors(x, y) {
-                self.count[nx + ny * self.width] += if new_state { 1 } else { -1 }
+    fn set_cell_state(&mut self, x: usize, y: usize, alive: bool) {
+        let index = x + y * self.width;
+        let delta = if alive {1} else {-1};
+        // let current = self.cells[index];
+        if self.cells[index] != alive {
+            self.cells[index] = alive;
+            self.last_change[index] = self.generation;
+            for (dx, dy) in self.neighborhood.iter() {
+                let (nx, ny) = self.wrap(&x, &y, dx, dy);
+                self.count[nx + ny * self.width] += delta;
             }
         }
     }
-    fn neighbors(&self, x: usize, y:usize) -> Vec<(usize, usize)> {
-        let mut neighbors = Vec::new();
-        for (dx, dy) in &self.neighborhood {
-            let nx = (((x + self.width) as i32 + dx) as usize)  % self.width;
-            let ny = (((y + self.height) as i32 + dy) as usize) % self.height;
-            neighbors.push((nx, ny))
+    fn wrap(&self, x: &usize, y: &usize, dx: &i32, dy: &i32) -> (usize, usize) {
+        ((((x + self.width) as i32 + dx) as usize) % self.width, (((y + self.height) as i32 + dy) as usize) % self.height)
+    }
+    pub fn tick(&mut self) {
+        self.generation += 1;
+        // let mut born = 0;
+        // let mut died = 0;
+        for index in 0..self.width * self.height {
+            self.delta[index] = match (self.cells[index], self.count[index]) {
+                (true, 2) | (true, 3) => 0,
+                (true, _) => -1,
+                (false, 3) => 1,
+                (false, _) => 0,
+            }
         }
-        neighbors
+        for y in 0..self.height {
+            let offset = y * self.width;
+            for x in 0..self.width {
+                let delta = self.delta[offset + x];
+                if delta != 0 {
+                    self.set_cell_state(x, y, if delta > 0 {true} else {false});
+                }
+            }
+        }
+    }
+    pub fn evaluate(&self) -> State {
+        State::Unknown
     }
     pub fn display(&self) {
         for y in 0..self.height {
              println!("{}", &self.cells[y*self.width..y*self.width+self.width].iter().map(|b|{if *b {'X'} else {' '}}).collect::<String>())
         }
-        for y in 0..self.height {
-            println!("{}", &self.count[y*self.width..y*self.width+self.width].iter().map(|c| char::from_digit(*c as u32, 10).unwrap()).collect::<String>())
-        }
+        // for y in 0..self.height {
+        //     println!("{}", &self.count[y*self.width..y*self.width+self.width].iter().map(|c| char::from_digit(*c as u32, 10).unwrap()).collect::<String>())
+        // }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 #![feature(assoc_char_funcs)]
+#![feature(allocator_api)]
 mod ca;
+
 use crate::ca::World;
+use std::time::SystemTime;
 
 fn main() {
+    let mut total = 0;
     let neighborhood = vec![(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
-    let mut world = World::new(100, 100, neighborhood);
-    world.randomize(0.3);
-    world.display();
+    let mut world = World::new(256, 256, neighborhood.clone());
+    for _ in 0..10 {
+        world.randomize(0.31);
+        let now = SystemTime::now();
+        for _ in 0..100 {
+            world.tick();
+            // world.display();
+            // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+        }
+        total += now.elapsed().unwrap().as_micros();
+    }
+    println!("{}", total/1000);
+    // for (i, v) in world.heatmap().iter().enumerate() {
+    //     println!("{}\t{}", (i as i32) - 128, v);
+    // }
 }
