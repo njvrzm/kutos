ca4f02f6ecdb6369ab4b2c3cdb3f7a3894c0bdf3
Nathan Verzemnieks <njvrzm@gmail.com>
2021-04-05 12:13:52 -0700

    Stripped-ish down version to highlight my borrow problem

--- src/ca.rs
+++ src/ca.rs
@@ -4,39 +4,29 @@ pub struct World {
     width: usize,
     height: usize,
     cells: Vec<bool>,
-    delta: Vec<i8>,
-    count: Vec<i8>,
-    last_change: Vec<i32>,
+    born: Vec<usize>,
+    died: Vec<usize>,
+    count: Vec<i32>,
     neighborhood: Vec<(i32, i32)>,
-    generation: i32,
-}
-
-pub enum State {
-    Unknown,
-    Interesting,
-    Dead,
-    Exploding,
+    generation: u64,
 }
 
 impl World {
     pub fn new(width: usize, height: usize, neighborhood: Vec<(i32, i32)>) -> Self {
+        let size = width * height;
         Self{
             width,
             height,
-            cells: vec![false; width * height],
-            delta: vec![0; width * height],
-            count: vec![0; width * height],
-            last_change: vec![0; width * height],
+            cells: vec![false; size],
+            born: Vec::with_capacity(size),
+            died: Vec::with_capacity(size),
+            count: vec![0; size],
             neighborhood,
             generation: 0,
         }
     }
-    pub fn reset(&mut self) {
-        *self = Self::new(self.width, self.height, self.neighborhood.clone());
-    }
     pub fn randomize(&mut self, fill: f32) {
         let mut rng = StdRng::seed_from_u64(32);
-        self.reset();
         for x in 0..self.width {
             for y in 0..self.height {
                 if fill > rng.sample(Standard) {
@@ -48,50 +38,47 @@ impl World {
     fn set_cell_state(&mut self, x: usize, y: usize, alive: bool) {
         let index = x + y * self.width;
         let delta = if alive {1} else {-1};
-        // let current = self.cells[index];
         if self.cells[index] != alive {
             self.cells[index] = alive;
-            self.last_change[index] = self.generation;
-            for (dx, dy) in self.neighborhood.iter() {
-                let (nx, ny) = self.wrap(&x, &y, dx, dy);
-                self.count[nx + ny * self.width] += delta;
+            for (dx, dy) in &self.neighborhood {
+                let mut nx = *dx + x as i32;
+                let mut ny = *dy + y as i32;
+                if nx < 0 {
+                    nx += self.width as i32
+                } else if nx > self.width as i32 {
+                    nx -= self.width as i32
+                }
+                if ny < 0 {
+                    ny += self.height as i32
+                } else if ny > self.height as i32 {
+                    ny -= self.height as i32
+                }
+
+                self.count[nx as usize + (ny as usize) * self.width] += delta;
             }
         }
-    }
-    fn wrap(&self, x: &usize, y: &usize, dx: &i32, dy: &i32) -> (usize, usize) {
-        ((((x + self.width) as i32 + dx) as usize) % self.width, (((y + self.height) as i32 + dy) as usize) % self.height)
     }
     pub fn tick(&mut self) {
         self.generation += 1;
-        // let mut born = 0;
-        // let mut died = 0;
+        self.born.clear();
+        self.died.clear();
         for index in 0..self.width * self.height {
-            self.delta[index] = match (self.cells[index], self.count[index]) {
-                (true, 2) | (true, 3) => 0,
-                (true, _) => -1,
-                (false, 3) => 1,
-                (false, _) => 0,
+            match (self.cells[index], self.count[index]) {
+                (true, 2) | (true, 3) => (),
+                (true, _) => self.died.push(index),
+                (false, 3) => self.born.push(index),
+                (false, _) => (),
             }
         }
-        for y in 0..self.height {
-            let offset = y * self.width;
-            for x in 0..self.width {
-                let delta = self.delta[offset + x];
-                if delta != 0 {
-                    self.set_cell_state(x, y, if delta > 0 {true} else {false});
-                }
-            }
+        // this works but defeats the purpose
+        //for index in self.died.clone() {
+        // This gives "cannot borrow `*self` as mutable because it is also borrowed as immutable"
+        for index in self.died.iter() {
+            self.set_cell_state(*index%self.width, *index/self.width, false);
         }
-    }
-    pub fn evaluate(&self) -> State {
-        State::Unknown
-    }
-    pub fn display(&self) {
-        for y in 0..self.height {
-             println!("{}", &self.cells[y*self.width..y*self.width+self.width].iter().map(|b|{if *b {'X'} else {' '}}).collect::<String>())
+        for index in self.born.iter() {
+            self.set_cell_state(*index%self.width, *index/self.width, true);
+
         }
-        // for y in 0..self.height {
-        //     println!("{}", &self.count[y*self.width..y*self.width+self.width].iter().map(|c| char::from_digit(*c as u32, 10).unwrap()).collect::<String>())
-        // }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,12 @@ use crate::ca::World;
 use std::time::SystemTime;
 
 fn main() {
-    let mut total = 0;
     let neighborhood = vec![(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)];
     let mut world = World::new(256, 256, neighborhood.clone());
-    for _ in 0..10 {
-        world.randomize(0.31);
-        let now = SystemTime::now();
-        for _ in 0..100 {
-            world.tick();
-            // world.display();
-            // print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-        }
-        total += now.elapsed().unwrap().as_micros();
+    world.randomize(0.31);
+    let now = SystemTime::now();
+    for _ in 0..1000 {
+        world.tick();
     }
-    println!("{}", total/1000);
-    // for (i, v) in world.heatmap().iter().enumerate() {
-    //     println!("{}\t{}", (i as i32) - 128, v);
-    // }
+    println!("{}", now.elapsed().unwrap().as_micros());
 }
